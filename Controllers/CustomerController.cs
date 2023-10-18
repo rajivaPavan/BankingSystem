9a1f52e5b47e364564fdd85694e5240a087eb9f1
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2023-10-18 13:57:01 +0530

    removed invalid controllers, views

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -9,40 +9,4 @@ public class CustomerController : Controller
     {
         
     }
-    
-    public IActionResult Index()
-    {
-        return View();
-    }
-    
-    public IActionResult Loan()
-    {
-        // database query
-        var loans = new List<LoanViewModel>
-        {
-            new LoanViewModel
-            {
-                Id = "1",
-                Name = "Home Loan",
-                Description = "Home Loan Description"
-            },
-            new LoanViewModel
-            {
-                Id = "2",
-                Name = "Car Loan",
-                Description = "Car Loan Description"
-            },
-            new LoanViewModel
-            {
-                Id = "3",
-                Name = "Personal Loan",
-                Description = "Personal Loan Description"
-            }
-        };
-        var allLoans = new AllLoansViewModel
-        {
-            Loans = loans
-        };
-        return View(allLoans);
-    }
 }

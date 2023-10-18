62db37e486a7b27b2fb99c6a1de11fdd3aca33fa
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2023-10-18 17:01:42 +0530

    added views for self registration

---
+++ ViewModels/SelfRegistration.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankingSystem.ViewModels;
+
+public class SelfRegistrationViewModel
+{
+    [Required]
+    public string NIC { get; set; } = string.Empty;
+    
+    [Required]
+    public string MobileNumber { get; set; } = string.Empty;
+}
+
+public class FinalizeSelfRegisterViewModel
+{
+    [Required]
+    public string Username { get; set; }
+    
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+    
+    [Required]
+    [DataType(DataType.Password)]
+    [Compare("Password")]
+    public string ConfirmPassword { get; set; }
+    
+    [Required]
+    public string OTP { get; set; }
+}

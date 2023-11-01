d60861e19d8397f717b7a06cc53b111bfc7ae28b
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2023-11-01 22:21:18 +0530

    updated loan installment report view

--- Migrations/20231031_LoanReports.sql
+++ Migrations/20231031_LoanReports.sql
@@ -24,21 +24,23 @@ DELIMITER ;
 -- creating a view for late loan installment report with having enough information.
 DROP VIEW IF EXISTS loan_installment_report;
 CREATE VIEW loan_installment_report AS
-SELECT 
-    ind.customer_id, 
-    ind.NIC, 
-    CONCAT(ind.first_name, ' ', ind.last_name) AS Name, -- combine first and last names
+SELECT
+    ind.customer_id,
+    ind.NIC,
+    MAX(CONCAT(ind.first_name, ' ', ind.last_name)) AS Name,
     l.loan_id,
-    MIN(li.due_date) AS last_unpaid_date, -- getting last paid date (minimum date) for calculate the installments and for display
-    SUM(CASE WHEN li.is_paid = 0 THEN 1 ELSE 0 END) AS no_of_missing_installments, -- counting how many installments having to pay
+    ba.branch_id,
+    MIN(li.due_date) AS last_unpaid_date,
+    SUM(CASE WHEN li.is_paid = 0 THEN 1 ELSE 0 END) AS no_of_missing_installments,
     lp.interest,
     l.total_amount,
     CalculateMissingAmount(l.total_amount, l.duration, lp.interest, SUM(CASE WHEN li.is_paid = 0 THEN 1 ELSE 0 END)) AS missing_amount
 FROM individual AS ind
-JOIN loan AS l ON l.customer_id = ind.customer_id
-JOIN loan_plan AS lp ON l.loan_plan_id = lp.loan_plan_id
-RIGHT JOIN loan_installment AS li ON l.loan_id = li.loan_id
-GROUP BY ind.customer_id, ind.NIC, l.loan_id;
+         JOIN loan AS l ON l.customer_id = ind.customer_id
+         JOIN loan_plan AS lp ON l.loan_plan_id = lp.loan_plan_id
+         JOIN bank_account AS ba ON ind.customer_id = ba.customer_id
+         RIGHT JOIN loan_installment AS li ON l.loan_id = li.loan_id
+GROUP BY ind.customer_id, ind.NIC, l.loan_id, ba.branch_id;
 
 
 

6e5e5784f7a25ee77e1b577e89b5ef82034dea0a
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2023-11-01 13:52:59 +0530

    changed the timestamp to date in fd date fields

---
+++ Migrations/20231101_1_FixedDeposits.sql
@@ -0,0 +1,6 @@
+﻿-- change type of fixed_deposit alter table fixed_deposit opening_date to date
+alter table fixed_deposit
+    modify opening_date date null;
+
+alter table fixed_deposit
+    modify next_calculation_on date null;
